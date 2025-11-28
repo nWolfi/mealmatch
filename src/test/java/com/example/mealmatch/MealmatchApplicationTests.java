7e1b924198a00ea0d071b5628b601b3d32b80a6e
W_lfle, Noel <noel.woelfle@sybit.de>
2025-11-28 09:42:14 +0100

    init

---
+++ src/test/java/com/example/mealmatch/MealmatchApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.mealmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
